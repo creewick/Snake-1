c51d38d6a0982637b2f7fb798f03c7c0436f8203
Илья Creewick Юхатский <creewick@outlook.com>
2017-11-28 22:54:56 +0500

    генерация лабиринта для комнаты

--- src/snake/levelGenerators/GridLevelGenerator.java
+++ src/snake/levelGenerators/GridLevelGenerator.java
@@ -20,7 +20,11 @@ public class GridLevelGenerator implements ILevelGenerator {
                                int applesToGenerateAmount) {
         this.width = width;
         this.height = height;
-        field = generateField(width, height);
+        field = new Field(width, height)
+                .addGrid()
+                .addPasses()
+                .addDoors(new Vector[]{new Vector(0, 0)})
+                .toArray();
 
         Snake snake = addSnake(1, 1);
         field[3][3] = new Apple();
@@ -31,54 +35,5 @@ public class GridLevelGenerator implements ILevelGenerator {
     private Snake addSnake(int x, int y) {
         field[y][x] = new SnakeHead(x, y, Direction.ZERO, null, null);
         return new Snake(x, y, Direction.ZERO);
-    }
-
-    private IFieldObject[][] generateField(int width, int height) {
-        IFieldObject[][] emptyField = new IFieldObject[height][width];
-        IFieldObject[][] fieldForEmptyGraph = fieldForEmptyGraph(emptyField);
-        IFieldObject[][] fieldWithTwoWays = fieldWithTwoWays(fieldForEmptyGraph);
-        return fieldWithTwoWays;
-    }
-
-    private Vector[] getTwoVectors(int x, int y) {
-        Vector[] result = new Vector[2];
-        Random random = new Random();
-        List<Vector> variants = new ArrayList<>();
-        if (x > 1)
-            variants.add(Direction.LEFT);
-        if (x < width - 2)
-            variants.add(Direction.RIGHT);
-        if (y > 1)
-            variants.add(Direction.TOP);
-        if (y < height - 2)
-            variants.add(Direction.BOTTOM);
-        result[0] = (variants.get(random.nextInt(variants.size())));
-        variants.remove(result[0]);
-        result[1] = (variants.get(random.nextInt(variants.size())));
-        return result;
-    }
-
-    private IFieldObject[][] fieldWithTwoWays(IFieldObject[][] field) {
-        for (int y = 1; y < height; y+=2){
-            for (int x = 1; x < width; x+=2){
-                for (Vector vector : getTwoVectors(x, y)) {
-                    field[y+vector.y][x+vector.x] = new Empty();
-                }
-            }
-        }
-        return field;
-    }
-
-    private IFieldObject[][] fieldForEmptyGraph(IFieldObject[][] field) {
-        for (int y = 0; y < height; y++){
-            for (int x = 0; x < width; x++){
-                if (x % 2 == 0 || y % 2 == 0){
-                    field[y][x] = new Wall();
-                } else {
-                    field[y][x] = new Empty();
-                }
-            }
-        }
-        return field;
     }
 }
--- src/snakeGUI/Program.java
+++ src/snakeGUI/Program.java
@@ -13,16 +13,16 @@ import java.lang.reflect.InvocationTargetException;
 public class Program {
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IOException, IllegalAccessException {
         Level[] levels = new Level[1];
-        // levels[0] = new GridLevelGenerator().generateLevel(30, 30, 1, 3);
+        levels[0] = new GridLevelGenerator().generateLevel(30, 30, 1, 3);
         //levels[0] = new RoomLevelGenerator().generateLevel(30, 30, 2, 5);
-        FieldReader fieldReader =
-                new FieldReader("level8.txt");
-        levels[0] = new Level(fieldReader, 1);
-        Pair<Portal, Portal> portals = Portal.generatePortals(
-                new Vector(3, 0),
-                new Vector(0, 0));
-        levels[0].setObjectOnField(portals.getKey().getPosition(), portals.getKey());
-        levels[0].setObjectOnField(portals.getValue().getPosition(), portals.getValue());
+//        FieldReader fieldReader =
+//                new FieldReader("level8.txt");
+//        levels[0] = new Level(fieldReader, 1);
+//        Pair<Portal, Portal> portals = Portal.generatePortals(
+//                new Vector(3, 0),
+//                new Vector(0, 0));
+//        levels[0].setObjectOnField(portals.getKey().getPosition(), portals.getKey());
+//        levels[0].setObjectOnField(portals.getValue().getPosition(), portals.getValue());
         Game game = new Game(levels);
         MainSnakeWindow window = new MainSnakeWindow(game);
     }
