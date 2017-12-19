2cc8249e8c8fd7003af524fee1008ab359d19c31
Vladimir <twnk@list.ru>
2017-12-19 14:12:25 +0500

    eto robotoet (net)

--- src/snakeGUI/Program.java
+++ src/snakeGUI/Program.java
@@ -8,10 +8,7 @@ import java.lang.reflect.InvocationTargetException;
 public class Program {
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException,
             IOException, IllegalAccessException {
-//        levels[0] = new GridLevelGenerator().generateLevel(60, 60, 1, 3);
-  //      levels[0] = new CombineLevelGenerator().generateLevel(60, 30, 2, 5);
-        Level level = new GameReader("level7.txt").loadLevel();
-        Game game = new Game(level);
+        Game game = GameReader.readLevel(7);
         new MainSnakeWindow().loadGame(game);
     }
 }
